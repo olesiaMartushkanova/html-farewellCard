1b0b2d317dff2ce300f8cf050d99f745079eb7f3
Olesia Martushkanova <302793@twgroup.co.nz>
2019-03-26 08:56:08 +1300

    First commit

---
+++ ali-task/test.js
@@ -0,0 +1,12 @@
+let inputElement = document.getElementById('input-text-field');
+let outputElement = document.getElementById('output-text-field');
+let buttonElement = document.getElementById('btn');
+
+let inputValue = inputElement.value;
+
+// buttonElement.onclick = function () {
+//     console.log(inputElement.value);
+// }
+buttonElement.onclick = function () {
+   let outputValue = outputElement.value;
+}
