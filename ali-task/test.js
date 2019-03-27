882363dfa50eaec17edbfc9f2f091454a8757177
Olesia Martushkanova <302793@twgroup.co.nz>
2019-03-27 13:04:07 +1300

    Fixed displaying value in the output field

--- ali-task/test.js
+++ ali-task/test.js
@@ -2,15 +2,11 @@ let inputElement = document.getElementById('input-text-field');
 let outputElement = document.getElementById('output-text-field');
 let buttonElement = document.getElementById('btn');
 
-let inputValue = inputElement.value;
+buttonElement.onclick = function () {
+    outputElement.value = inputElement.value;
+    }
 
 //Log value entered in the first field in the console
 // buttonElement.onclick = function () {
 //     console.log(inputElement.value);
 // }
-
-//Log value entered in the first field in another field
-//This is not working
-buttonElement.onclick = function () {
-outputElement.innerHTML = inputValue;
-}
